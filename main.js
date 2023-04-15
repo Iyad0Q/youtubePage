78d65f8efa8d1bdd3ddc7bb28de1549aeb10fef8
Iyad <eyad6823@gmail.com>
2023-04-15 20:34:05 +0300

    -

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// vars
 let nav = document.querySelector("nav");
 let button = document.querySelector("#menu");
 let contatiner = document.querySelector(".videos-container");
 
+// functions
 button.addEventListener("click", function() {
     nav.classList.toggle("active");
     button.classList.toggle("active");
